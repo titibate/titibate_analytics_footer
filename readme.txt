e001a8bbb8585740ad04bc60e54769a1381c6157
fernandoherlo <fernando.herlo@gmail.com>
2019-03-08 08:43:32 +0100

    added more information

--- readme.txt
+++ readme.txt
@@ -3,3 +3,15 @@ Contributors: titibate
 Tags: analytics, footer
 License: GPL3
 License URI: https://www.gnu.org/licenses/gpl.html
+
+== Description ==
+Add analytics tracking code in the footer page.
+
+== Installation ==
+
+Installing "Analytics in the footer - Titibate" can be done either by searching for "Analytics in the footer" via the "Plugins > Add New" screen in your WordPress dashboard, or by using the following steps:
+
+1. Download the plugin via WordPress.org
+2. Upload the ZIP file through the 'Plugins > Add New > Upload' screen in your WordPress dashboard
+3. Activate the plugin through the 'Plugins' menu in WordPress
+4. Add a new line to your config file: define( 'WP_TTB_ANALYTICS', 'UA-XXXXXXXX-X' );
--- titibate_analytics_footer.php
+++ titibate_analytics_footer.php
@@ -1,29 +1,35 @@
 <?php
-/*
-Plugin Name: Analytics in the footer - Titibate
-Description: Add analytics tracking code in the footer page
-Version: 0.1
-Author: Titibate
-Author URI: https://titibate.com
-License: GPL3
-License URI: https://www.gnu.org/licenses/gpl.html
-*/
+/**
+ * @link              https://github.com/titibate/titibate_analytics_footer
+ * @since             0.1.0
+ * @package           titibate_analytics_footer
+ *
+ * @wordpress-plugin
+ * Plugin Name: Analytics in the footer - Titibate
+ * Description: Add analytics tracking code in the footer page
+ * Version: 0.1
+ * Author: Titibate
+ * Author URI: https://titibate.com
+ * License: GPL3
+ * License URI: https://www.gnu.org/licenses/gpl.html
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
  
 // HOOK footer
-add_action("wp_footer", "ttb_afooter");
+add_action('wp_footer', 'ttb_afooter');
  
 // FUNCTION
 function ttb_afooter()
 {
-	if (defined('WP_TTB_ANALYTICS')) {
-	  echo '<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=' . WP_TTB_ANALYTICS . '"></script>
-	<script>
-	  window.dataLayer = window.dataLayer || [];
-	  function gtag(){dataLayer.push(arguments);}
-	  gtag("js", new Date());
-
-	  gtag("config", "' . WP_TTB_ANALYTICS . '");
-	</script>';
-	}
+  if (defined('WP_TTB_ANALYTICS')) {
+    echo '<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=' . WP_TTB_ANALYTICS . '"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag("js", new Date());
+      gtag("config", "' . WP_TTB_ANALYTICS . '");
+    </script>';
+  }
 }
